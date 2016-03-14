7f49642e14b34504fc1f063b2983105366fac531
Dan Morphis <dan@milkcarton.com>
2016-03-13 19:31:54 -0800

    Remove test log message

--- src/Main.py
+++ src/Main.py
@@ -21,11 +21,6 @@ def setup_logging():
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     ch.setFormatter(formatter)
     logger.addHandler(ch)
-    logger.debug('debug message')
-    logger.info('info message')
-    logger.warn('warn message')
-    logger.error('error message')
-    logger.critical('critical message')
 
 def main():
     """Main"""
